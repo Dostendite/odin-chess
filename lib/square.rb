d6af5e3c4c23651066a554e43820bd209d1bf3f1
Dostendite <dostendite@outlook.es>
2024-08-16 02:27:20 +0200

    New: Add Square class initializer & methods
    The square takes in a color & coordinate,
    and provides the empty? and black? predicate
    methods.

--- lib/square.rb
+++ lib/square.rb
@@ -1,15 +1,39 @@
+require "rainbow"
+
+# square class
+# -- job is to provide the board with building
+# -- blocks and store the pieces within
 class Square
+  attr_reader :coordinate, :color
+
   def initialize(color, coordinate)
     @color = color
     @coordinate = coordinate
     @piece = nil
   end
 
-  def has_piece?
-    !piece.nil?
+  def empty?
+    @piece.nil?
   end
 
   def black?
     @color == "black"
+  end
+  
+  def to_s
+    if empty?
+      "  "
+    else
+      "#{@piece} "
+    end
+  end
+
+  def print_self
+    if black?
+      print Rainbow(to_s).bg("#1375d1") if black?
+    else
+      print Rainbow(to_s).bg("#ffffff")
+    end
+    # puts if @coordinate.include?("h") && !@coordinate.include?("1")
   end
 end
