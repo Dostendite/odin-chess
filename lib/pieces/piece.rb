38d2bb0d050b81c11fed3fe224eef838f4725564
Dostendite <dostendite@outlook.es>
2024-08-23 06:07:48 +0200

    Refactor: methods to calculate possible squares
    The now calculate_squares method has now been tested
    inside the game and proven to work, all that's left is
    to add castling, checks, en passant, stale, and checkmate.

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -24,104 +24,51 @@ class Piece
     !(0..7).include?(x) || !(0..7).include?(y)
   end
 
-  def allied_piece?(square)
-    square.piece.color == @color
-  end
-
+  # when one is zero, the one doesn't work
+  # but both work if they're negative
   def get_valid_squares(board)
     valid_squares = []
     
-    if !@axes[:x].nil?
-      valid_squares += calculate_squares_x(board, @axes[:x])
-      valid_squares += calculate_squares_x(board, -@axes[:x])
+    unless @axes[:x].nil?
+      valid_squares += calculate_squares(board, @axes[:x], 0)
+      valid_squares += calculate_squares(board, -@axes[:x], 0)
     end
   
-    if !@axes[:y].nil?
-      valid_squares += calculate_squares_y(board, @axes[:y])
-      valid_squares += calculate_squares_y(board, -@axes[:y])
+    unless @axes[:y].nil?
+      valid_squares += calculate_squares(board, 0, @axes[:y])
+      valid_squares += calculate_squares(board, 0, -@axes[:y])
     end
   
-    if !@axes[:d].nil?
-      valid_squares += calculate_squares_d(board, @axes[:d], @axes[:d])
-      valid_squares += calculate_squares_d(board, @axes[:d], -@axes[:d])
-      valid_squares += calculate_squares_d(board, -@axes[:d], @axes[:d])
-      valid_squares += calculate_squares_d(board, -@axes[:d], -@axes[:d])
+    unless @axes[:d].nil?
+      valid_squares += calculate_squares(board, @axes[:d], @axes[:d])
+      valid_squares += calculate_squares(board, @axes[:d], -@axes[:d])
+      valid_squares += calculate_squares(board, -@axes[:d], @axes[:d])
+      valid_squares += calculate_squares(board, -@axes[:d], -@axes[:d])
     end
     valid_squares
   end
 
-  def calculate_squares_x(board, x_delta)
+  def calculate_squares(board, x_delta, y_delta)
     row, column = @position
     squares = []
+  
+    1.upto([x_delta.abs, y_delta.abs].max) do |range|
 
-    1.upto(x_delta.abs) do |delta|
-      delta = -delta if x_delta.negative?
-
-      break if out_of_bounds?(row + delta, column)
-      
-      target_square = board[row + delta][column]
-
-      # make this into a predicate method or something
-      if !target_square.empty?
-        if allied_piece?(target_square)
-          break
-        else
-          # opposing piece found
-          squares << target_square
-          break
-        end
+      unless x_delta.zero?
+        x_delta = x_delta.negative? ? -range : range
       end
-      squares << target_square
-    end
-    squares
-  end
 
-  def calculate_squares_y(board, y_delta)
-    row, column = @position
-    squares = []
-
-    1.upto(y_delta.abs) do |delta|
-      delta = -delta if y_delta.negative?
-    
-      break if out_of_bounds?(row, column + delta)
-      
-      target_square = board[row][column + delta]
-
-      if !target_square.empty?
-        if allied_piece?(target_square)
-          break
-        else
-          # opposing piece found
-          squares << target_square
-          break
-        end
+      unless y_delta.zero?
+        y_delta = y_delta.negative? ? -range : range
       end
-      squares << target_square
-    end
-    squares
-  end
 
-  def calculate_squares_d(board, x_delta, y_delta)
-    row, column = @position
-    squares = []
-    1.upto([x_delta, y_delta].max.abs) do |delta_amount|
-      x_delta = x_delta.negative? ? -delta_amount : delta_amount
-      y_delta = y_delta.negative? ? -delta_amount : delta_amount
-
-      break if out_of_bounds?(row + x_delta.abs, column + y_delta.abs)
+      break if out_of_bounds?(row + x_delta, column + y_delta)
 
       target_square = board[row + x_delta][column + y_delta]
-      # piece found
-      if !target_square.empty?
-        if allied_piece?(target_square)
-          break
-        else
-          # opposing piece found
-          squares << target_square
-          break
-        end
-      end
+      break if target_square.friendly?(@color)
+
       squares << target_square
+      break if target_square.opposing?(@color)
     end
     squares
   end
