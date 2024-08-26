22e31e6a4891ff5f718c32a8fe52af64147be489
Dostendite <dostendite@outlook.es>
2024-08-26 02:09:13 +0200

    Tests: Write tests for pawn movement & captures

--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -1,3 +1,4 @@
+require_relative "../lib/board"
 require_relative "../lib/pieces/piece"
 
 require_relative "../lib/pieces/pawn"
@@ -14,7 +15,7 @@ RSpec.describe Piece do
   describe "#black?" do
     it "returns true when the square color is 'Black'" do
       # is this a bad practice? do research
-      # maybe I should use let
+      # maybe I should use let instead
       black_piece = Piece.new("Black", [2, 4])
       black_check = black_piece.black?
       expect(black_check).to be true
@@ -31,6 +32,7 @@ RSpec.describe Piece do
     it "returns true when x is out of bounds" do
       x = -1
       y = 5
+
       bounds_check = piece.out_of_bounds?(x, y)
       expect(bounds_check).to be true
     end
@@ -54,6 +56,7 @@ RSpec.describe Piece do
     it "returns false when both are in bounds" do
       x = 5
       y = 0
+
       bounds_check = piece.out_of_bounds?(x, y)
       expect(bounds_check).to be false
     end
@@ -61,8 +64,128 @@ RSpec.describe Piece do
     it "returns false when both are in bounds (II)" do
       y = 7
       x = 1
+
       bounds_check = piece.out_of_bounds?(x, y)
       expect(bounds_check).to be false
     end
   end
+end
+
+# tests for all the chess pieces
+
+# board editor link used:
+# https://lichess.org/editor/8/8/8/8/8/8/8/8_w_-_-_0_1?color=white
+
+# they are written in this order:
+# 1. valid move squares
+# 2. blocked squares 
+# 3. capture squares
+
+RSpec.describe Pawn do
+  describe "#get_valid_squares" do
+    it "returns two squares ahead when the pawn hasn't moved" do
+      # can't use a before block because the variables aren't
+      # in scope, do more research
+      chess_board = Board.new
+      chess_board.board = chess_board.generate_board
+      test_board = chess_board.board
+
+      pawn = Pawn.new("White", [1, 4])
+      valid_squares = pawn.get_valid_squares(test_board)
+
+      expect(valid_squares.length).to be(2)
+    end
+
+    it "returns one square ahead when the pawn has moved" do
+      chess_board = Board.new
+      chess_board.board = chess_board.generate_board
+      test_board = chess_board.board
+
+      pawn = Pawn.new("White", [1, 4])
+      pawn.instance_variable_set(:@moved, true)
+
+      valid_squares = pawn.get_valid_squares(test_board)
+
+      expect(valid_squares.length).to be(1)
+    end
+
+    it "returns no squares when a piece is one square ahead" do
+      chess_board = Board.new
+      chess_board.board = chess_board.generate_board
+      test_board = chess_board.board
+
+      opposing_piece = Pawn.new("White", [5, 4])
+      chess_board.add_piece(opposing_piece, opposing_piece.position)
+
+      pawn = Pawn.new("Black", [6, 4])
+      valid_squares = pawn.get_valid_squares(test_board)
+
+      expect(valid_squares).to be_empty
+    end
+
+    it "returns one square ahead when a piece is two squares ahead" do
+      chess_board = Board.new
+      chess_board.board = chess_board.generate_board
+      test_board = chess_board.board
+
+      opposing_piece = Pawn.new("White", [4, 4])
+      chess_board.add_piece(opposing_piece, opposing_piece.position)
+
+      pawn = Pawn.new("Black", [6, 4])
+      valid_squares = pawn.get_valid_squares(test_board)
+
+      expect(valid_squares.length).to be(1)
+    end
+
+    it "returns a diagonal move when a piece is one diagonal square ahead" do
+      chess_board = Board.new
+      chess_board.board = chess_board.generate_board
+      test_board = chess_board.board
+
+      opposing_piece = Pawn.new("White", [5, 5])
+      chess_board.add_piece(opposing_piece, opposing_piece.position)
+
+      pawn = Pawn.new("Black", [6, 4])
+      valid_squares = pawn.get_valid_squares(test_board)
+
+      # 2 peaceful moves + 1 attacking
+      expect(valid_squares.length).to be(3)
+    end
+
+    it "returns two diagonal moves when two pieces are diagonal squares ahead" do
+      chess_board = Board.new
+      chess_board.board = chess_board.generate_board
+      test_board = chess_board.board
+
+      opposing_piece_left = Pawn.new("White", [5, 3])
+      chess_board.add_piece(opposing_piece_left, opposing_piece_left.position)
+
+      opposing_piece_right = Pawn.new("White", [5, 5])
+      chess_board.add_piece(opposing_piece_right, opposing_piece_right.position)
+
+      pawn = Pawn.new("Black", [6, 4])
+      valid_squares = pawn.get_valid_squares(test_board)
+
+      expect(valid_squares.length).to be(4)
+    end
+  end
+end
+
+RSpec.describe Bishop do
+end
+
+RSpec.describe Knight do
+  
+end
+
+RSpec.describe Rook do
+  
+end
+
+RSpec.describe Queen do
+  
+end
+
+RSpec.describe King do
+  
 end
