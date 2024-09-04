6835329fb6eca6f441bdb41c45a4ec9439db7337
Dostendite <dostendite@outlook.es>
2024-09-04 22:12:32 +0200

    Tests: Remove unnecessary imports

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,17 +1,5 @@
 require_relative "../lib/board"
 require_relative "../lib/chess"
-require_relative "../lib/square"
-require_relative "../lib/display"
-require_relative "../lib/serializer"
-require_relative "../lib/move_validator"
-
-require_relative "../lib/pieces/piece.rb"
-require_relative "../lib/pieces/pawn.rb"
-require_relative "../lib/pieces/knight.rb"
-require_relative "../lib/pieces/bishop.rb"
-require_relative "../lib/pieces/rook.rb"
-require_relative "../lib/pieces/queen.rb"
-require_relative "../lib/pieces/king.rb"
 
 # use bin/rspec to run rspec
 RSpec.describe Chess do
