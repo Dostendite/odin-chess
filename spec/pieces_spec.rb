299ff5d9cca7277ef55b6a1767eca57c563266fb
Dostendite <dostendite@outlook.es>
2024-09-04 22:13:45 +0200

    Tests: Change import spacing

--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -1,6 +1,6 @@
 require_relative "../lib/board"
-require_relative "../lib/pieces/piece"
 
+require_relative "../lib/pieces/piece"
 require_relative "../lib/pieces/pawn"
 require_relative "../lib/pieces/knight"
 require_relative "../lib/pieces/bishop"
